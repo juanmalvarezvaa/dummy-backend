8fb1db380717b2778847015af553f9cff98668b8
Juan <juan.alvarez.vaa@gmail.com>
2023-12-31 15:25:38 -0300

    Update readme, remove unused wait

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dummy-backend
  This is a demo Backend for the React Demo Poster app, using Node.js and Express.js
+## Getting Started
+
+Go the root folder of the project, and run the following commands:
+
+```bash
+npm start
+```
+
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.get("/posts", async (req, res) => {
   const storedPosts = await getStoredPosts();
-  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500)); // Simulate slow network
+  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500)); // Simulate slow network
   res.json({ posts: storedPosts });
 });
 
